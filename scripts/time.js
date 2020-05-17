04b4e2f508184d9e0c6e7bcaa6b6b9242364d261
Curtis Bridges <curtis@curtisbridges.com>
2020-05-17 12:33:31 -0400

    add correct salutation
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- scripts/time.js
+++ scripts/time.js
@@ -19,4 +19,21 @@ function updateTime() {
 
     document.getElementById('date-time').innerHTML = `${timeString} ${dateString}`;
     setTimeout(function () { updateTime() }, 60000);
+
+    document.getElementById('greeting-header').innerHTML = getSalutation(currentDate, 'Curtis');
+
+
+}
+
+function getSalutation(currentDate = new Date(), name = 'Curtis') {
+    const hour = currentDate.getHours();
+    if (hour < 12) {
+        return `Good morning, ${name}`;
+    } else if (hour < 16) {
+        return `Good afternoon, ${name}`;
+    } else if (hour < 24) {
+        return `Good evening, ${name}`;
+    } else {
+        return `Hello, ${name}`;
+    }
 }
