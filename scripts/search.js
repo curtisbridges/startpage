9e766771c4fb0aaa23e9f459666ca749d98dbc6a
Curtis Bridges <curtis@curtisbridges.com>
2020-05-20 10:30:01 -0400

    add icons to links
    Signed-off-by: Curtis Bridges <curtis@curtisbridges.com>

--- scripts/search.js
+++ scripts/search.js
@@ -1,11 +1,11 @@
 window.onload = (event) => {
-    const search = document.getElementById('search-input')
-    search.focus()
+    const search = document.getElementById('search-input');
+    search.focus();
 
     search.addEventListener('keydown', event => {
         if (event.keyCode === 13) {
-            console.log('going')
-            window.location.href = `https://www.google.com/search?q=${search.value}`
+            console.log('going');
+            window.location.href = `https://www.google.com/search?q=${search.value}`;
         }
     });
 };
